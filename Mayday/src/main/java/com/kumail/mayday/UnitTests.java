3eec24b0e50433420aee108ab33f5e885da55536
skumailraza <14beseskumailraza@seecs.edu.pk>
2017-06-02 09:49:46 +0500

    A test added

--- Mayday/src/main/java/com/kumail/mayday/UnitTests.java
+++ Mayday/src/main/java/com/kumail/mayday/UnitTests.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.AndroidTestCase;
 import android.test.InstrumentationTestCase;
 import android.test.IsolatedContext;
 import android.test.mock.MockContext;
@@ -117,6 +121,29 @@ public class UnitTests {
         }
 
     }
+
+    @RunWith(AndroidJUnit4.class)
+    public class GPSPollingServiceTest extends AndroidTestCase {
+        private LocationManager locationManager;
+        @Test
+        public void testGPS() {
+            LocationManager locationManager = (LocationManager) this.getContext().getSystemService(Context.LOCATION_SERVICE);
+            locationManager.addTestProvider("Test", false, false, false, false, false, false, false, Criteria.POWER_LOW, Criteria.ACCURACY_FINE);
+            locationManager.setTestProviderEnabled("Test", true);
+
+            // Set up your test
+
+            Location location = new Location("Test");
+            location.setLatitude(10.0);
+            location.setLongitude(20.0);
+            locationManager.setTestProviderLocation("Test", location);
+
+            // Check if your listener reacted the right way
+
+            locationManager.removeTestProvider("Test");
+        }
+    }
+
 
 
 }
